8d9602ac331f531e9e6ffa048e73c817445c6b60
Aastha Sarkar <aasthasarkar13gul@gmail.com>
2020-12-04 22:40:47 +0530

    Designing admin panel/ create product and assignment

--- projfrontend/src/index.js
+++ projfrontend/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Routes from './Routes'
+import React from "react";
+import Routes from "./Routes";
+import ReactDOM from "react-dom";
 
-ReactDom.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById("root"));
